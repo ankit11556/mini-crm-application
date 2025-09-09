5eb1275a8b752b88c34910f4fad8d623d31d7fe9
ankit <saharan442090@gmail.com>
2025-09-09 12:10:21 +0530

    correct model spelling

---
+++ backend/models/Customer.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+name: {
+  type: String,
+  required: true
+},
+phone: {
+  type: String
+},
+company: {
+  type: String
+},
+ownerId: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: "User",
+  required: true
+}
+},{timestamps: true});
+
+const Customer = mongoose.model("Customer", customerSchema);
+module.exports = Customer
