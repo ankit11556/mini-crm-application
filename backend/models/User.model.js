2fad7c938da2eed2a1e87ebd37b7b9e7d25905ec
ankit <saharan442090@gmail.com>
2025-09-09 11:56:40 +0530

    create user schema

---
+++ backend/models/User.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+name: {
+  type: String,
+  required: true
+},
+email: {
+  type: String, 
+  required: true,
+  unique: true
+},
+passwordHash: {
+  type: String,
+  required: true
+},
+role: {
+  type: String,
+  enum: ["Admin","User"],
+  default: "User"
+}
+},{timestamps: true})
+
+const User = mongoose.model("User",userSchema);
+module.exports = User
