d0d2032097ded9ccde4509583c4207f648ddbc2c
ankit <saharan442090@gmail.com>
2025-09-14 19:54:45 +0530

    create check-auth route

--- backend/routes/Auth.routes.js
+++ backend/routes/Auth.routes.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const {registerController,loginController,logout} = require('../controllers/User.controller')
+const {protect} = require('../middleware/Auth.middleware')
+
+router.get("/check-auth",protect,(req,res)=>{
+  res.json({user: req.user})
+})
 
 router.post("/register",registerController)
 router.post("/login",loginController)
