77aac98a4a9550d72fbe568094b54943337e3233
ankit <saharan442090@gmail.com>
2025-09-14 17:35:12 +0530

    create logout contoller & route

--- backend/routes/Auth.routes.js
+++ backend/routes/Auth.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {registerController,loginController} = require('../controllers/User.controller')
+const {registerController,loginController,logout} = require('../controllers/User.controller')
 
 router.post("/register",registerController)
 router.post("/login",loginController)
+router.post("/logout",logout)
 
 module.exports = router
